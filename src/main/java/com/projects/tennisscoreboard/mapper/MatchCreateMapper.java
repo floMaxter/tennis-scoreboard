560bd056732d538b01fbad1c16cc155897b9eb56
Max Vdovin <maxpostupaet@gmail.com>
2025-03-14 10:10:00 +0300

    feat: add the name conversion on the appropriate format in MatchCreateMapper.
    - The method removes unnecessary spaces at the beginning ant end of the name, and also leaves one space between the words.

--- src/main/java/com/projects/tennisscoreboard/mapper/MatchCreateMapper.java
+++ src/main/java/com/projects/tennisscoreboard/mapper/MatchCreateMapper.java
@@ -3,6 +3,8 @@ package com.projects.tennisscoreboard.mapper;
 import com.projects.tennisscoreboard.dto.MatchCreateDto;
 import jakarta.servlet.http.HttpServletRequest;
 
+import java.util.Optional;
+
 public class MatchCreateMapper implements Mapper<HttpServletRequest, MatchCreateDto> {
 
     private static final MatchCreateMapper INSTANCE = new MatchCreateMapper();
@@ -12,8 +14,17 @@ public class MatchCreateMapper implements Mapper<HttpServletRequest, MatchCreate
 
     @Override
     public MatchCreateDto mapFrom(HttpServletRequest object) {
-        return new MatchCreateDto(object.getParameter("firstPlayerName"),
-                object.getParameter("secondPlayerName"));
+        return new MatchCreateDto(normalizeName(
+                object.getParameter("firstPlayerName")),
+                normalizeName(object.getParameter("secondPlayerName"))
+        );
+    }
+
+    private String normalizeName(String name) {
+        return Optional.ofNullable(name)
+                .map(String::trim)
+                .map(str -> str.replaceAll("\\s+", " "))
+                .orElse("");
     }
 
     public static MatchCreateMapper getInstance() {
