3adb606f6fdcfa4d1fb968ba7890f38651a1b065
Max Vdovin <maxpostupaet@gmail.com>
2025-03-19 16:26:26 +0300

    fix: delete bidirectional Many-to-One associate between Player and Match.

--- src/main/java/com/projects/tennisscoreboard/entity/Player.java
+++ src/main/java/com/projects/tennisscoreboard/entity/Player.java
@@ -5,24 +5,16 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
-import lombok.ToString;
-
-import java.util.ArrayList;
-import java.util.List;
 
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@ToString(exclude = "matches")
-@EqualsAndHashCode(of = "name")
 @Entity
 @Builder
 @Table(name = "t_players")
@@ -34,10 +26,6 @@ public class Player {
 
     @Column(name = "c_name", nullable = false, unique = true)
     private String name;
-
-    @Builder.Default
-    @OneToMany(mappedBy = "match")
-    private List<Match> matches = new ArrayList<>();
 
     public Player(String name) {
         this.name = name;
