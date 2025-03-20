59f2187180ccfc4cadb187e4c62ee516831c92dc
Max Vdovin <maxpostupaet@gmail.com>
2025-03-20 14:33:57 +0300

    feat: add page for completed matchers. Add logic for searching completed matches on servlet layer.

--- src/main/java/com/projects/tennisscoreboard/controller/MatchesController.java
+++ src/main/java/com/projects/tennisscoreboard/controller/MatchesController.java
@@ -1,6 +1,7 @@
 package com.projects.tennisscoreboard.controller;
 
 import com.projects.tennisscoreboard.Utils.JspHelper;
+import com.projects.tennisscoreboard.service.FinishedMatchesPersistenceService;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -12,8 +13,14 @@ import java.io.IOException;
 @WebServlet("/matches")
 public class MatchesController extends HttpServlet {
 
+    private final FinishedMatchesPersistenceService finishedMatchesPersistenceService = FinishedMatchesPersistenceService.getInstance();
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        var playerName = req.getParameter("name");
+        var matchesByPlayerName = finishedMatchesPersistenceService.findAllByPlayerName(playerName);
+
+        req.setAttribute("matches", matchesByPlayerName);
         req.getRequestDispatcher(JspHelper.getPath("matches"))
                 .forward(req, resp);
     }
--- src/main/webapp/WEB-INF/jsp/matches.jsp
+++ src/main/webapp/WEB-INF/jsp/matches.jsp
@@ -1,9 +1,27 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+
+<c:set var="matches" value="${requestScope.matches}"/>
+
 <html>
     <head>
-        <title>Title</title>
+        <title>Completed matches</title>
     </head>
     <body>
-        <h1>All matches</h1>
+        <h1>Completed matches</h1>
+        <table>
+            <tr>
+                <th>First player</th>
+                <th>Second player</th>
+                <th>Winner</th>
+            </tr>
+            <c:forEach var="match" items="${matches}">
+                <tr>
+                    <td>${match.firstPlayer.name}</td>
+                    <td>${match.secondPlayer.name}</td>
+                    <td>${match.winner.name}</td>
+                </tr>
+            </c:forEach>
+        </table>
     </body>
 </html>
