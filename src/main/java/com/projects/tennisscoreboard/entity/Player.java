92d545390c5c5093d2f34d35cfb0e5becd40fee3
Max Vdovin <maxpostupaet@gmail.com>
2025-04-10 13:20:49 +0300

    fix: remove Data annotation in Entity

--- src/main/java/com/projects/tennisscoreboard/entity/Player.java
+++ src/main/java/com/projects/tennisscoreboard/entity/Player.java
@@ -8,11 +8,15 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 
-@Data
+@Getter
+@Setter
+@ToString
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
