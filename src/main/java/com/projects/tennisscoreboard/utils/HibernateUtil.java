e80c57df522e17309907b50e4143d20d9e2fde32
Max Vdovin <maxpostupaet@gmail.com>
2025-04-10 18:56:44 +0300

    chore: add db initialization using hibernate

--- src/main/java/com/projects/tennisscoreboard/utils/HibernateUtil.java
+++ src/main/java/com/projects/tennisscoreboard/utils/HibernateUtil.java
@@ -4,9 +4,11 @@ import com.projects.tennisscoreboard.entity.Match;
 import com.projects.tennisscoreboard.entity.Player;
 import lombok.Getter;
 import lombok.experimental.UtilityClass;
+import lombok.extern.slf4j.Slf4j;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+@Slf4j
 @UtilityClass
 public class HibernateUtil {
 
@@ -15,12 +17,12 @@ public class HibernateUtil {
 
     private static SessionFactory buildSessionFactory() {
         try {
-            Configuration configuration = new Configuration();
-            configuration.addAnnotatedClass(Match.class);
-            configuration.addAnnotatedClass(Player.class);
-            configuration.configure();
+            Configuration configuration = new Configuration()
+                    .addAnnotatedClass(Match.class)
+                    .addAnnotatedClass(Player.class);
             return configuration.buildSessionFactory();
         } catch (Throwable e) {
+            log.error("SessionFactory initialization error");
             throw new ExceptionInInitializerError(e);
         }
     }
--- src/main/resources/db/tennis_scoreboard_init_data.sql
+++ src/main/resources/db/tennis_scoreboard_init_data.sql
@@ -4,6 +4,6 @@ values ('Max'),
        ('Oleg');
 
 insert into t_matches(c_first_player_id, c_second_player_id, c_winner_id)
-values ( 1, 2, 1 ),
+values (1, 2, 1),
        (2, 3, 2),
        (1, 3, 3);
